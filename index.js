a51b3011eacd24d02c86474d3e8725132e9a813e
Ryan Hanthorn <blitzsturm@gmail.com>
2019-09-01 16:56:43 -0500

    corrected for cascade delete not working

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function Main()
 	if (!process.env.PORT) process.env.PORT = 5000;
 	if (!process.env.DB_PATH) process.env.DB_PATH = './log.db';
 
-	var db, db_err, esc = s => "'" + s.replace(/'/g, "''") + "'";
+	var db, db_err, esc = s => "'" + (s || "").replace(/'/g, "''") + "'";
 	try
 	{
 		db = await cfgDB(process.env.DB_PATH);
@@ -32,76 +32,47 @@ async function Main()
 	app.get("/api/log", (req, res) =>
 	{
 		if (db_err) throw db_err;
-		if (req.query.key)
+		db.all(`
+			SELECT l.time, l.data
+			FROM log l LEFT JOIN keys k ON l.keyid = k.id
+			WHERE k.key = ${esc(req.query.key)}
+			ORDER BY l.time DESC
+			LIMIT 1000
+		`, (e, r) =>
 		{
-			db.all(`
-				SELECT l.time, l.data
-				FROM log l LEFT JOIN keys k ON l.keyid = k.id
-				WHERE k.key = ${esc(req.query.key)}
-				ORDER BY l.time DESC
-				LIMIT 1000
-			`, (e, r) =>
-			{
-				if (e) throw e;
-				res.json(r);
-			});
-		}
-		else
-		{
-			db.all(`
-				SELECT l.time, l.data
-				FROM log l
-				WHERE l.keyid IS NULL
-				ORDER BY l.time DESC
-				LIMIT 1000
-			`, (e, r) =>
-			{
-				if (e) throw e;
-				res.json(r);
-			});
-		}
+			if (e) throw e;
+			res.json(r);
+		});
+
 	});
 
 	app.get("/api/write", (req, res) =>
 	{
 		if (db_err) throw db_err;
 		var row = {time: new Date().getTime(), data: req.query.msg};
-		if (req.query.key)
+		db.exec(`
+			INSERT OR IGNORE INTO keys (key) VALUES (${esc(req.query.key)});
+			INSERT INTO log (time, keyid, data) VALUES (${row.time}, (SELECT id FROM keys WHERE key = ${esc(req.query.key)}), ${esc(row.data)});
+		`, e =>
 		{
-			db.exec(`
-				INSERT OR IGNORE INTO keys (key) VALUES (${esc(req.query.key)});
-				INSERT INTO log (time, keyid, data) VALUES (${row.time}, (SELECT id FROM keys WHERE key = ${esc(req.query.key)}), ${esc(row.data)});
-			`, e =>
-			{
-				if (e) throw e;
-				res.json(row);
-			});
-		}
-		else
-		{
-			db.exec(`INSERT INTO log (time,data) VALUES (${row.time},${esc(row.data)});`, e =>
-			{
-				if (e) throw e;
-				res.json(row);
-			});
-		}
+			if (e) throw e;
+			res.json(row);
+		});
+		
 	});
 
 	app.get("/api/delete", (req, res) =>
 	{
 		if (db_err) throw db_err;
-		if (req.query.key)
+		db.exec(`
+			DELETE FROM log WHERE keyid = (SELECT id FROM keys WHERE key = ${esc(req.query.key)});
+			DELETE FROM keys WHERE key = ${esc(req.query.key)};
+			VACUUM;
+		`, e =>
 		{
-			db.exec(`DELETE FROM keys WHERE key = ${esc(req.query.key)}; VACUUM;`, e =>
-			{
-				if (e) throw e;
-				res.json({status:"success"});
-			});
-		}
-		else
-		{
-			res.json({status:"failure", message: "no key provided"});
-		}
+			if (e) throw e;
+			res.json({status:"success"});
+		});
 	});
 
 	app.get("/api/status", (req, res) =>
@@ -141,7 +112,7 @@ function cfgDB(strPath)
 				time INTEGER,
 				keyid INTEGER,
 				data TEXT,
-				FOREIGN KEY (keyid) REFERENCES keys(id) ON DELETE CASCADE
+				FOREIGN KEY (keyid) REFERENCES keys (id) ON DELETE CASCADE
 			);
 			CREATE INDEX IF NOT EXISTS ix_log_time ON log (time DESC);
 		`, e => e ? reject(e) : resolve(db)));
